a1e4e92c82bcf5949310a0f3eb4c1cb1bc58e8bb
btdjangbah001 <btdjangbah001@st.ug.edu.gh>
2023-04-10 23:36:33 +0000

    added simple frontend to help test the app

---
+++ chat-app-fe/chat.js
@@ -0,0 +1,58 @@
+const chatForm = document.querySelector(".chat-form");
+const messageInput = document.getElementById("message-input");
+const receiverId = document.getElementById("receiver-input");
+const token = localStorage.getItem("token");
+const user = JSON.parse(localStorage.getItem("user"));
+
+const ws = new WebSocket(`ws://localhost:8080/chat/${token}`);
+
+ws.addEventListener("open", () => {
+  console.log("Connected to WebSocket");
+});
+
+ws.addEventListener("close", () => {
+  console.log("Disconnected from WebSocket");
+});
+
+ws.addEventListener("message", (event) => {
+  console.log(`Message received: ${event.data}`);
+  const message = JSON.parse(event.data);
+
+  addMessage(message);
+});
+
+chatForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const content = messageInput.value;
+  messageInput.value = "";
+
+  const message = {
+    content: content,
+    sender_id: user.id,
+    sender_username: user.username,
+    recipient_id: +receiverId.value,
+    type: 0,
+  };
+
+  addMessage(message, "You");
+
+  ws.send(JSON.stringify(message));
+});
+
+// Get the chat box and form elements
+const chatBox = document.querySelector(".chat-box");
+
+// Function to add a message to the chat box
+function addMessage(message, sender = "") {
+  const messageElement = document.createElement("div");
+  console.log(message.content);
+
+  messageElement.className = "chat-message";
+  messageElement.innerHTML = `<span>${
+    !sender ? message.sender_username : sender
+  }:</span> ${message.content}`;
+
+  chatBox.appendChild(messageElement);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
---
+++ chat-app-fe/index.js
@@ -0,0 +1,60 @@
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+const url = "http://localhost:8080";
+
+signupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById("signup-username").value;
+  const password = document.getElementById("signup-password").value;
+  const email = document.getElementById("signup-email").value;
+
+  fetch(`${url}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Redirect to chat page
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      window.location.href = "/chat.html";
+    })
+    .catch((error) => console.error(error));
+});
+
+loginForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById("login-username").value;
+  const password = document.getElementById("login-password").value;
+  const email = document.getElementById("login-email").value;
+
+  const res = await fetch(`${url}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_field: username,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      // Redirect to chat page
+      window.location.href = "/chat.html";
+    })
+    .catch((error) => console.error(error));
+});
